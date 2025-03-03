f63a39193241112583fc27ed05075763e15380e2
Vlad Suciu <suciu.vlad@gmail.com>
2025-03-03 08:51:26 +0200

    adsF

--- packages/ui/src/input/input.tsx
+++ packages/ui/src/input/input.tsx
@@ -16,13 +16,14 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "border-input file:text-foreground placeholder:text-muted-foreground/70 flex h-10 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-sm shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
+          "bg-transparent",
+          "border-input file:text-foreground placeholder:text-muted-foreground/70 flex h-10 w-full min-w-0 rounded-md border px-3 py-1 text-sm shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
           "focus-visible:ring-1 focus-visible:ring-inset focus-visible:ring-ring focus-visible:border-ring focus-visible:outline-none",
           "aria-invalid:ring-destructive dark:aria-invalid:ring-destructive aria-invalid:border-destructive",
           type === "search" &&
             "[&::-webkit-search-cancel-button]:appearance-none [&::-webkit-search-decoration]:appearance-none [&::-webkit-search-results-button]:appearance-none [&::-webkit-search-results-decoration]:appearance-none",
           type === "file" &&
-            "text-muted-foreground/70 file:border-input file:text-foreground p-0 pr-3 italic file:me-3 file:h-full file:border-0 file:border-r file:border-solid file:bg-transparent file:px-3 file:text-sm file:font-medium file:not-italic",
+            "file:bg-transparent text-muted-foreground/70 file:border-input file:text-foreground p-0 pr-3 italic file:me-3 file:h-full file:border-0 file:border-r file:border-solid file:px-3 file:text-sm file:font-medium file:not-italic",
           className
         )}
         ref={ref}
